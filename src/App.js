4dedd18ed7a1029edcc28dd42a6ca9ad45e7c00a
Raushan Sharma01~ <raushankumar7485852533@gmail.com>
2023-05-27 21:13:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import  React  from 'react';
 import QuizeQue from './components/QuizeQue';
-
+ 
 function App() {
     return (
-    <div className="App">
+     <div className="App">
       <h1>React Quiz </h1>
        <QuizeQue/>
       
     </div>
-  );
+   );
 }
 
 export default App;
